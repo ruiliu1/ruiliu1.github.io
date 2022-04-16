d5f7918322466fcdab8b0064eec5abb184d17f03
ruiliu1 <ruiliu1@cs.stonybrook.edu>
2022-04-15 21:35:18 -0400

    Initial Commit

---
+++ Bullseye/BullseyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullseyeApp.swift
+//  Bullseye
+//
+//  Created by Rui Liu on 4/15/22.
+//
+
+import SwiftUI
+
+@main
+struct BullseyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
